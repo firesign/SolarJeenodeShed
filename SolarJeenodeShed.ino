39877083134bf447c72864f72f2c107533371f21
Michael LeBlanc <michaelleblanc1@me.com>
2021-07-17 16:55:41 -0300

    Update SolarJeenodeShed.ino
    Changed to SHT-21 T & H sensor from DHT-22 for better performance. Also updated movingaverage library/functions.

--- SolarJeenodeShed.ino
+++ SolarJeenodeShed.ino
@@ -1,36 +1,46 @@
 // Solar Jeenode -- Shed (node 5) June 27, 2016
 // Burn to device as Arduino Mini w/ATMEGA328
+// Replaced DHT with SHT21 module for better performance
 
 #include <JeeLib.h>
+#include <Ports.h>
 #include <avr/sleep.h>
 #include <movingAvg.h>
 
-movingAvg avgSolarPanel;
-movingAvg avgBattery;
+movingAvg avgSolarPanel(4);
+movingAvg avgBattery(4);
 
 long bvAvg, spAvg;
 
-DHTxx dht (3);          // DHT connected to pin D3
+#include <Wire.h>
+#include <Sodaq_SHT2x.h>
+
+//  DHTxx dht (3);          // DHT connected to pin D3
 #define SEND_MODE 2     // set to 3 if fuses are e=06/h=DE/l=CE, else set to 2
 
-int batteryPin = 1;     // Battery to A1 
+int batteryPin = 1;     // Battery to A1
 int spPin = 0;          // Solar Panel to A0
 
 struct {
   int temp;	    // temperature
   int hum;	    // humidity
-  long bv;          // battery voltage
-  long spv;         // solar panel voltage
-} 
+  long bv;      // battery voltage
+  long spv;     // solar panel voltage
+}
 payload;
 
 // this must be defined since we're using the watchdog for low-power waiting
-ISR(WDT_vect) { 
-  Sleepy::watchdogEvent(); 
-} 
+ISR(WDT_vect) {
+  Sleepy::watchdogEvent();
+}
 
 void setup() {
-//  Serial.begin(9600);
+  //Serial.begin(9600);
+  Wire.begin();
+
+  avgSolarPanel.begin();
+  avgBattery.begin();
+
   rf12_initialize(5, RF12_433MHZ, 212); // 433 Mhz, net group 212, node 11
   rf12_control(0xC040); 		// set low-battery level to 2.2V i.s.o. 3.1V
   rf12_sleep(RF12_SLEEP);
@@ -48,36 +58,38 @@ static byte sendPayload () {
 
 void loop() {
 
-  int t, h;
-  
-  if (dht.reading(t, h, true)) {
-//    Serial.print("\ntemperature = ");
-//    Serial.println(t);
-//    Serial.print("humidity = ");
-//    Serial.println(h);
+  float t, h;
+
+  //if (dht.reading(t, h, true)) {
+  if (SHT2x.GetHumidity(), true) {
+    h = SHT2x.GetHumidity() * 10;
+    t = SHT2x.GetTemperature() * 10;
   }
+  //  Serial.print("\ntemperature = ");
+  //  Serial.println(t);
+  //  Serial.print("humidity = ");
+  //  Serial.println(h);
+  //}
 
   long b_v = analogRead(batteryPin);
   bvAvg = avgBattery.reading(b_v);
   long sp_v = analogRead(spPin);
   spAvg = avgSolarPanel.reading(sp_v);
 
-//  Serial.print("Battery Voltage (raw): ");
-//  Serial.println(b_v);
-//  Serial.print("Battery Voltage (Volts): ");
-//  Serial.println(b_v * 3.3 / 1024);
-//  Serial.flush();
-  
+  //  Serial.print("Battery Voltage (raw): ");
+  //  Serial.println(b_v);
+  //  Serial.print("Battery Voltage (Volts): ");
+  //  Serial.println(b_v * 3.3 / 1024);
+  //  Serial.flush();
+
   payload.bv = bvAvg;
   payload.spv = spAvg;
-  
+
   payload.temp = t;
-  payload.hum = h; 
+  payload.hum = h;
   sendPayload();
 
-  for (byte i = 0; i < 3; ++i) 
+  for (byte i = 0; i < 3; ++i) {
     Sleepy::loseSomeTime(30000); //wake up after 90 seconds
+  }
 }
-
-
-
